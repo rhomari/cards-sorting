db109d9305fbc7019f7559da7a0ccea9db6c0707
TARIQ RHOMARI <tariqrhomari@gmail.com>
2022-04-05 00:40:08 +0000

    .

--- core.js
+++ core.js
@@ -17,21 +17,24 @@ function waitforit(time){
     cardsinfos.sort((x,y)=> (x.value<y.value ?-1:1))
     
     
-     cardsinfos.forEach(async(e, i)=>{
+     cardsinfos.forEach(async(e, i)=> {
         
-        
-        document.documentElement.style.setProperty('--xpos',-Math.random()*100 * i+'px')
-        document.documentElement.style.setProperty('--ypos', -Math.random()*200* i+'px')
+        setTimeout(()=>{
+            document.documentElement.style.setProperty('--xpos',-Math.random()*100 * i+'px')
+            document.documentElement.style.setProperty('--ypos', -Math.random()*200* i+'px')
+           
+            cards[i].classList.add('cardrotate');
+            
+            cards[i].innerText=e.content;
+            
+            cards[i].setAttribute('value', e.value);
+            
+            
+            
+
+        },i*100)
        
-        cards[i].classList.add('cardrotate');
-        
-        cards[i].innerText=e.content;
-        
-        cards[i].setAttribute('value', e.value);
-        await waitforit(2000);
-        
         cards[i].classList.remove('cardrotate');
-       
         
     })
     
