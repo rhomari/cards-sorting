e60733eb109f18dc08e1181d0ec76e6d50a23502
TARIQ RHOMARI <tariqrhomari@gmail.com>
2022-04-05 00:28:18 +0000

    .

--- core.js
+++ core.js
@@ -2,7 +2,7 @@
 function waitforit(time){
     return new Promise(resolve => setTimeout(resolve, time));
 }
- function randomSort(){
+ function Sort(){
     const cards= document.querySelectorAll('.card');
     console.table(cards);
     
@@ -17,13 +17,12 @@ function waitforit(time){
     cardsinfos.sort((x,y)=> (x.value<y.value ?-1:1))
     
     
-    cardsinfos.forEach(async(e, i)=>{
+     cardsinfos.forEach(async(e, i)=>{
         
         
         document.documentElement.style.setProperty('--xpos',-Math.random()*100 * i+'px')
         document.documentElement.style.setProperty('--ypos', -Math.random()*200* i+'px')
-        document.documentElement.style.setProperty('--xposdest',e.x + 'px')
-        document.documentElement.style.setProperty('--yposdest', e.y+'px')
+       
         cards[i].classList.add('cardrotate');
         
         cards[i].innerText=e.content;
